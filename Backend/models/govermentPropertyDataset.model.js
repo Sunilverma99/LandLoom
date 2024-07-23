5475976cb760e445d250ff7564c7074f4bab554d
Sunil <sv784333@gmial.com>
2024-07-23 16:49:51 +0530

    Goverment property Schema

---
+++ Backend/models/govermentPropertyDataset.model.js
@@ -0,0 +1,63 @@
+import mongoose,{Schema} from "mongoose";
+
+const propertySchema = new Schema({
+    propertyId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    propertyType:{
+        type:String,
+        required:true,
+    },
+    propertyLocation:{
+        type:String,
+        required:true,
+    },
+    propertyOwner:{
+        type:String,
+        required:true,
+    },
+    propertyOwnerContact:{
+        type:String,
+        required:true,
+    },
+    propertyOwnerEmail:{
+        type:String,
+        required:true,
+    },
+    propertyOwnerAddress:{
+        type:String,
+        required:true,
+    },
+    propertySize:{
+        type:String,
+        required:true,
+    },
+    // propertyOwnerAadhar:{
+    //     type:String,
+    //     required:true,
+    // },
+    // propertyOwnerPan:{
+    //     type:String,
+    //     required:true,
+    // },
+    propertyPhotos:{
+        type:Array,
+        required:true,
+    },
+    propertyDocuments:{
+        type:Array,
+        required:true,
+    },
+    propertyWaterAndElectrityConnection:{
+      type:Boolean,
+      default:true,
+    }
+},
+{
+    timestamps: true,
+  }
+);
+const GovermentPropertyDataset = mongoose.model("GovermentPropertyDataset",propertySchema);
+export default GovermentPropertyDataset;  
