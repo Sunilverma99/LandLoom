2577fc7ba392381b5fd46856443b3c55cb994ac0
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-07-28 14:32:12 +0530

    Update npm dependencies and add Leaflet and React Leaflet packages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,7 @@ root.render(
     <BrowserRouter>
       <Navbar />
       <App />
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
